d96753feef089193393d42b3c424d28060562edb
ricfrank <riccardo.franconi@gmail.com>
2012-07-21 16:48:04 +0200

    generata nuova versione

---
+++ migration-scripts/Version20120721164657.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TestMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20120721164657 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
